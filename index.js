ebcfbc0ecdd1becf5284665247912bfc8dbff367
Rhishav <rhishh.me@gmail.com>
2024-06-27 17:21:32 +0530

    game window loaded

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
-// gsap.from(".load-text",{
-//   opacity:0.3,
-//   duration:1.2,
-//   repeat: Infinity,
-//   ease:"bounce.in",
-//   yoyo:true
-// })
-// var tl= gsap.timeline();
-// tl.to("#loader",{
-//   opacity:0,
-//   scale:3,
-//   duration:1,
-//   delay:2.5,
-//   display:"none"
-// })
-// tl.from(".footer",{
-//   y:300,
-//   duration:0.5
-// })
-// tl.from("#ceiling-img",{
-//   y:-200,
-//   duration:0.5
-// })
-// tl.from("#hangmans-img",{
-//   opacity:0,
-//   y:-200,
-//   duration:1
-// })
-// tl.from(".title",{
-//   scale:0,
-//   duration:1,
-//   ease: "elastic.out(1,0.3)"
-// })
-// tl.from( "#in",{
-//   y:10,
-//   opacity:0,
-//   duration:0.5
-// }).call(noloader);
-// function noloader(){
-//   const loader=document.querySelector("#loader")
-//   loader.style.display="none"
-// }
-gameload()
+gsap.from(".load-text",{
+  opacity:0.3,
+  duration:1.2,
+  repeat: Infinity,
+  ease:"bounce.in",
+  yoyo:true
+})
+var tl= gsap.timeline();
+tl.to("#loader",{
+  opacity:0,
+  scale:3,
+  duration:1,
+  delay:2.5,
+  display:"none"
+})
+tl.from(".footer",{
+  y:300,
+  duration:0.5
+})
+tl.from("#ceiling-img",{
+  y:-200,
+  duration:0.5
+})
+tl.from("#hangmans-img",{
+  opacity:0,
+  y:-200,
+  duration:1
+})
+tl.from(".title",{
+  scale:0,
+  duration:1,
+  ease: "elastic.out(1,0.3)"
+})
+tl.from( "#in",{
+  y:10,
+  opacity:0,
+  duration:0.5
+}).call(noloader);
+function noloader(){
+  const loader=document.querySelector("#loader")
+  loader.style.display="none"
+}
+
+
 function gameload(){
   gsap.to("#hangmans-img",{
     y:-600,
@@ -85,7 +86,7 @@ function gameload(){
 
   gsap.to(".game-main",{
     duration:0.5,
-    delay:0.5,
+    delay:1,
     display:"flex"
   })
 }
